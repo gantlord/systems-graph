169c8fab29d1c4029cae6042f70ca220750a801d
Robin Bruce <robin.bruce@gmail.com>
2023-02-25 21:42:57 +0000

    setup now installs docker images from scratch - includes helper script to wipe docker state

---
+++ scripts/wipe_docker.sh
@@ -0,0 +1,3 @@
+sudo docker ps | tail -n +2 | awk '{print $1}' | while read cont; do sudo docker stop $cont; done
+sudo docker ps -a | tail -n +2 | awk '{print $1}' | while read cont; do sudo docker rm $cont; done
+sudo docker images | tail -n +2 | awk '{print $3}' | while read img; do sudo docker rmi $img; done
--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
-sudo docker run -p 8529:8529 -e ARANGO_NO_AUTH=1 arangodb/arangodb:3.10.3 > /tmp/oot 2>&1&
-sleep 10
+ARANGO_VERSION=latest
+sudo docker pull arangodb:$ARANGO_VERSION
+sudo docker run -p 8529:8529 -e ARANGO_NO_AUTH=1 arangodb/arangodb:$ARANGO_VERSION > /tmp/oot 2>&1&
+sleep 22
 CONTAINER=`sudo docker ps | awk '{print $1}' | tail -n 1`
 sudo docker cp data $CONTAINER:/tmp
 sudo docker exec -it $CONTAINER arangorestore --server.authentication false /tmp/data/db
 sudo docker ps
+
