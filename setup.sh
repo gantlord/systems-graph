56777c12151762b37a21529d69cb3e0f44c22ce4
gantlord <robin.bruce@gmail.com>
2023-02-25 23:51:44 +0100

    First restore (#1)
    can install the whole thing from scratch on a fresh AWS instance - all other instances terminate, this branch was the only state for the project at the time of merging
    
    * adding data for restore
    * able to create and restore to a fresh docker container
    * setup installs docker images from scratch - includes helper script to wipe docker state
    * added some git setup commands

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
-git config --global credential.credentialStore gpg
+ARANGO_VERSION=latest
+sudo yum -y install docker
+sudo systemctl start docker
+sudo docker pull arangodb:$ARANGO_VERSION
+sudo docker run -p 8529:8529 -e ARANGO_NO_AUTH=1 arangodb/arangodb:$ARANGO_VERSION > /tmp/oot 2>&1&
+sleep 22
+CONTAINER=`sudo docker ps | awk '{print $1}' | tail -n 1`
+sudo docker cp data $CONTAINER:/tmp
+sudo docker exec -it $CONTAINER arangorestore --server.authentication false /tmp/data/db
+sudo docker ps
 
