dded8eef0a3eb042727a18b35d969b4f59867160
Robin Bruce <robin.bruce@gmail.com>
2023-02-25 21:05:03 +0000

    able to create and restore to a fresh docker container

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
-git config --global credential.credentialStore gpg
-
+sudo docker run -p 8529:8529 -e ARANGO_NO_AUTH=1 arangodb/arangodb:3.10.3 > /tmp/oot 2>&1&
+sleep 10
+CONTAINER=`sudo docker ps | awk '{print $1}' | tail -n 1`
+sudo docker cp data $CONTAINER:/tmp
+sudo docker exec -it $CONTAINER arangorestore --server.authentication false /tmp/data/db
+sudo docker ps
