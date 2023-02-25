f7585929677cff15167b7dfdebc1ced9c2d4f5cd
Robin Bruce <robin.bruce@gmail.com>
2023-02-25 21:58:12 +0000

    adding docker daemon commands

--- scripts/wipe_docker_state.sh
+++ scripts/wipe_docker_state.sh
@@ -1,3 +1,4 @@
+sudo systemctl start docker
 sudo docker ps | tail -n +2 | awk '{print $1}' | while read cont; do sudo docker stop $cont; done
 sudo docker ps -a | tail -n +2 | awk '{print $1}' | while read cont; do sudo docker rm $cont; done
 sudo docker images | tail -n +2 | awk '{print $3}' | while read img; do sudo docker rmi $img; done
