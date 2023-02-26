395e1416712da186bbbee984a770101e1c7c9466
Robin Bruce <robin.bruce@gmail.com>
2023-02-26 18:19:27 +0000

    print out all documents in test program

--- go/main.go
+++ go/main.go
@@ -3,45 +3,62 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 
 	driver "github.com/arangodb/go-driver"
-	"github.com/arangodb/go-driver/http"
+	http "github.com/arangodb/go-driver/http"
 )
 
 func main() {
-	// Set up a connection to the ArangoDB instance.
+	// Set up a connection to the database
 	conn, err := http.NewConnection(http.ConnectionConfig{
 		Endpoints: []string{"http://localhost:8529"},
 	})
 	if err != nil {
-		log.Fatalf("Failed to set up connection to ArangoDB: %v", err)
+		panic(err)
 	}
 
-	// Set up authentication for the connection.
+	// Replace "username" and "password" with your ArangoDB credentials
 	client, err := driver.NewClient(driver.ClientConfig{
 		Connection:     conn,
 		Authentication: driver.BasicAuthentication("username", "password"),
 	})
 	if err != nil {
-		log.Fatalf("Failed to set up authentication for ArangoDB connection: %v", err)
+		panic(err)
 	}
 
-	// Get a database from the ArangoDB client.
-	db, err := client.Database(nil, "_system")
-	if err != nil {
-		log.Fatalf("Failed to get database from ArangoDB client: %v", err)
-	}
-	
-	// List the collections in the ArangoDB database.
-	ctx := context.Background()
-	collections, err := db.Collections(ctx)
-	if err != nil {
-		log.Fatalf("Failed to list collections in ArangoDB database: %v", err)
-	}
+	// Retrieve the collections you want to print documents from
+	collectionNames := []string{"binaries", "firewallRules", "nodes", "edges", "pods", "components", "people", "purposes"}
 
-	for _, collection := range collections {
-		fmt.Printf("Collection: %s\n", collection.Name())
+	// Iterate over the collections and print out all documents in each collection
+	for _, collName := range collectionNames {
+		// Retrieve the database
+		db, err := client.Database(nil, "_system")
+		if err != nil {
+			panic(err)
+		}
+
+		// Set up a query to retrieve all documents in the collection
+		ctx := context.Background()
+		query := "FOR doc IN @@collection RETURN doc"
+		bindVars := map[string]interface{}{
+			"@collection": collName,
+		}
+		cursor, err := db.Query(ctx, query, bindVars)
+		if err != nil {
+			panic(err)
+		}
+
+		// Iterate over the cursor and print out each document
+		var doc interface{}
+		for {
+			_, err := cursor.ReadDocument(ctx, &doc)
+			if driver.IsNoMoreDocuments(err) {
+				break
+			} else if err != nil {
+				panic(err)
+			}
+			fmt.Printf("%v\n", doc)
+		}
 	}
 }
 
