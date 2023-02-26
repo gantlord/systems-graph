030cc4ca750e3f175028bb95f599f9dd634eb4fa
Robin Bruce <robin.bruce@gmail.com>
2023-02-26 13:03:26 +0000

    first run at go working

--- scripts/setup_go.sh
+++ scripts/setup_go.sh
@@ -1,3 +1,3 @@
-sudo yum -y install go
-cd go ; go get github.com/arangodb/go-driver ; cd -
-cd go ; go mod init test ; go build ; ./test ; cd -
+sudo yum -y install go; sleep 1
+cd go ; go get github.com/arangodb/go-driver ; cd -; sleep 1
+cd go ; go mod init test ; go build ; ./test ; cd -; sleep 1
