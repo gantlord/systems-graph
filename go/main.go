6a468c128de41fb5ec9163e40c0fb6503ccb0b98
Robin Bruce <robin.bruce@gmail.com>
2023-02-27 22:16:58 +0000

    refactored the generation code

--- go/main.go
+++ go/main.go
@@ -1,176 +1,93 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"math/rand"
-	"test/utilities"
+    "context"
+    "fmt"
+    "math/rand"
+    "test/utilities"
 
-	driver "github.com/arangodb/go-driver"
-	http "github.com/arangodb/go-driver/http"
+    driver "github.com/arangodb/go-driver"
+    http "github.com/arangodb/go-driver/http"
 )
 
-var smallCollectionSize = 100
-var mediumCollectionSize = smallCollectionSize * 10
-var largeCollectionSize = mediumCollectionSize * 10
-
-func main() {
-	conn, err := http.NewConnection(http.ConnectionConfig{
-		Endpoints: []string{"http://localhost:8529"},
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	// Replace "username" and "password" with your ArangoDB credentials
-	client, err := driver.NewClient(driver.ClientConfig{
-		Connection:     conn,
-		Authentication: driver.BasicAuthentication("username", "password"),
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	db, err := client.Database(nil, "_system")
-
-	collectionNames := []string{"binaries", "firewallRules", "physicalServers", "edges", "abstractServers", "components", "people", "purposes"}
-
-	for _, collName := range collectionNames {
-
-		if coll, err := db.Collection(nil, collName); err == nil {
-			coll.Remove(nil)
-		}
-
-		ctx := context.Background()
-		options := &driver.CreateCollectionOptions{}
-		_, err := db.CreateCollection(ctx, collName, options)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	coll, err := db.Collection(context.Background(), "binaries")
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < smallCollectionSize; i++ {
-		doc := map[string]interface{}{
-			"_key":  fmt.Sprintf("binary%d", i),
-		}
-		_, err := coll.CreateDocument(context.Background(), doc)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	coll, err = db.Collection(context.Background(), "firewallRules")
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < smallCollectionSize; i++ {
-		doc := map[string]interface{}{
-			"_key":  fmt.Sprintf("rule%d", i),
-		}
-		_, err := coll.CreateDocument(context.Background(), doc)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	coll, err = db.Collection(context.Background(), "abstractServers")
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < smallCollectionSize; i++ {
-		doc := map[string]interface{}{
-			"_key":  fmt.Sprintf("abstractServers%d", i),
-		}
-		_, err := coll.CreateDocument(context.Background(), doc)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	coll, err = db.Collection(context.Background(), "components")
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < largeCollectionSize; i++ {
-		doc := map[string]interface{}{
-			"_key":  fmt.Sprintf("component%d", i),
-		}
-		_, err := coll.CreateDocument(context.Background(), doc)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	coll, err = db.Collection(context.Background(), "purposes")
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < smallCollectionSize; i++ {
-		doc := map[string]interface{}{
-			"_key":  fmt.Sprintf("purpose%d", i),
-		}
-		_, err := coll.CreateDocument(context.Background(), doc)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	coll, err = db.Collection(context.Background(), "people")
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < smallCollectionSize; i++ {
-		doc := map[string]interface{}{
-			"_key": fmt.Sprintf("%s%d", utilities.GetRandomName(), i),
-		}
-		_, err := coll.CreateDocument(context.Background(), doc)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	coll, err = db.Collection(context.Background(), "physicalServers")
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < mediumCollectionSize; i++ {
-		cores := rand.Intn(32) 
-		doc := map[string]interface{}{
-			"_key":  fmt.Sprintf("server%d", i),
-			"cores": cores,
-		}
-		_, err := coll.CreateDocument(context.Background(), doc)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-
-	for _, collName := range collectionNames {
-		ctx := context.Background()
-		collection, err := db.Collection(ctx, collName)
-		if err != nil {
-		    panic(err)
-		}
-
-		count, err := collection.Count(ctx)
-		if err != nil {
-		    panic(err)
-		}
-
-		fmt.Printf("The number of documents in %s is %d\n", collName, count)
-
-	}
+type CollectionInfo struct {
+    Name     string
+    Size     int
+    KeyGenFn func(i int) string
+    DocGenFn func(i int) map[string]interface{}
 }
 
+var collections = []CollectionInfo{
+    {"binaries", 100, func(i int) string { return fmt.Sprintf("binary%d", i) }, func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("binary%d", i)} }},
+    {"firewallRules", 100, func(i int) string { return fmt.Sprintf("rule%d", i) }, func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("rule%d", i)} }},
+    {"abstractServers", 100, func(i int) string { return fmt.Sprintf("abstractServers%d", i) }, func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("abstractServers%d", i)} }},
+    {"components", 1000, func(i int) string { return fmt.Sprintf("component%d", i) }, func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("component%d", i)} }},
+    {"purposes", 100, func(i int) string { return fmt.Sprintf("purpose%d", i) }, func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("purpose%d", i)} }},
+    {"people", 100, func(i int) string { return fmt.Sprintf("%s%d", utilities.GetRandomName(), i) }, func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("%s%d", utilities.GetRandomName(), i)} }},
+    {"physicalServers", 1000, func(i int) string { return fmt.Sprintf("server%d", i) }, func(i int) map[string]interface{} {
+        cores := rand.Intn(32)
+        return map[string]interface{}{"_key": fmt.Sprintf("server%d", i), "cores": cores}
+    }},
+}
+
+func main() {
+    conn, err := http.NewConnection(http.ConnectionConfig{
+        Endpoints: []string{"http://localhost:8529"},
+    })
+    if err != nil {
+        panic(err)
+    }
+
+    // Replace "username" and "password" with your ArangoDB credentials
+    client, err := driver.NewClient(driver.ClientConfig{
+        Connection:     conn,
+        Authentication: driver.BasicAuthentication("username", "password"),
+    })
+    if err != nil {
+        panic(err)
+    }
+
+    db, err := client.Database(nil, "_system")
+    if err != nil {
+        panic(err)
+    }
+
+    for _, collInfo := range collections {
+        if coll, err := db.Collection(nil, collInfo.Name); err == nil {
+            coll.Remove(nil)
+        }
+
+        ctx := context.Background()
+        options := &driver.CreateCollectionOptions{}
+        _, err := db.CreateCollection(ctx, collInfo.Name, options)
+        if err != nil {
+            panic(err)
+        }
+
+        coll, err := db.Collection(context.Background(), collInfo.Name)
+        if err != nil {
+            panic(err)
+        }
+
+        for i := 0; i < collInfo.Size; i++ {
+            doc := collInfo.DocGenFn(i)
+            _, err := coll.CreateDocument(context.Background(), doc)
+	    if err != nil {
+	        panic(err)
+	    }
+        }
+
+
+	collection, err := db.Collection(ctx, collInfo.Name)
+	if err != nil {
+	    panic(err)
+	}
+	count, err := collection.Count(ctx)
+	if err != nil {
+	    panic(err)
+	}
+
+	fmt.Printf("The number of documents in %s is %d\n", collInfo.Name, count)
+    }
+
+ }
