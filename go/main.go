8dbdbc5abb28f62e30bdb61dd09687a2f2899e8f
gantlord <robin.bruce@gmail.com>
2023-03-01 20:51:24 +0100

    random component connectivity functioning (#6)

--- go/main.go
+++ go/main.go
@@ -16,18 +16,24 @@ type CollectionInfo struct {
     DocGenFn func(i int) map[string]interface{}
 }
 
+var small = 10
+var medium = 10 * small
+var large = 10 * medium
+
 var collections = []CollectionInfo{
-    {"binaries", 1000,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("binary%d", i)} }},
-    {"firewallRules", 100,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("rule%d", i)} }},
-    {"abstractServers", 1000,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("abstractServers%d", i)} }},
-    {"components", 10000,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("component%d", i)} }},
-    {"purposes", 100,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("purpose%d", i)} }},
-    {"people", 100,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("%s%d", utilities.GetRandomName(), i)} }},
-    {"physicalServers", 1000,  func(i int) map[string]interface{} {
+    {"binaries", medium,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("binary%d", i)} }},
+    {"firewallRules", small,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("rule%d", i)} }},
+    {"abstractServers", medium,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("abstractServers%d", i)} }},
+    {"components", large,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("component%d", i)} }},
+    {"purposes", medium,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("purpose%d", i)} }},
+    {"people", medium,  func(i int) map[string]interface{} { return map[string]interface{}{"_key": fmt.Sprintf("%s%d", utilities.GetRandomName(), i)} }},
+    {"physicalServers", medium,  func(i int) map[string]interface{} {
         cores := rand.Intn(32)
         return map[string]interface{}{"_key": fmt.Sprintf("server%d", i), "cores": cores}
     }},
 }
+
+var connectionPct = 50
 
 func main() {
     conn, err := http.NewConnection(http.ConnectionConfig{
@@ -89,4 +95,132 @@ func main() {
 	fmt.Printf("The number of documents in %s is %d\n", collInfo.Name, count)
     }
 
+
+    // Randomly connect nodes without creating a cycle
+	rand.Seed(0)
+
+	edgeCollName := "edges"
+
+        if edgeColl, err := db.Collection(nil, edgeCollName); err == nil {
+            edgeColl.Remove(nil)
+        }
+	// Create the edge collection if it doesn't exist
+	opts := &driver.CreateCollectionOptions{
+		Type: driver.CollectionTypeEdge,
+	}
+	edgeColl, err := db.CreateCollection(context.Background(), edgeCollName, opts)
+	if err != nil {
+		panic(err)
+	}
+
+	// Get all nodes in the collection
+	var nodes []string
+	query := fmt.Sprintf("FOR node IN %s RETURN node._id", "components")
+	cursor, err := db.Query(context.Background(), query, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer cursor.Close()
+	for {
+		var node string
+		_, err := cursor.ReadDocument(context.Background(), &node)
+		if driver.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		nodes = append(nodes, node)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Randomly connect nodes
+	for i := range nodes {
+		for {
+			if rand.Intn(100) < connectionPct {
+				j := rand.Intn(len(nodes)) 
+				if i == j {
+					continue
+				}
+				//fmt.Printf("i=%d, j=%d\n", i, j)
+				// Check if the connection creates a cycle
+				if !createsCycle(db, "edges", nodes[i], nodes[j]) {
+
+					edge := map[string]interface{}{
+						"_from": fmt.Sprintf("%s", nodes[i]),
+						"_to":   fmt.Sprintf("%s", nodes[j]),
+					}
+					_, err := edgeColl.CreateDocument(context.Background(), edge)
+					//§fmt.Println(edge)
+					if err != nil {
+						panic(err)
+					}
+				}
+			} else {
+				break
+			}
+		}
+
+	}
+
+	// Count the number of subgraphs in the nodes
+	var visitedNodes = make(map[string]bool)
+	var subgraphCount int
+	for _, node := range nodes {
+		if !visitedNodes[node] {
+			subgraphCount++
+			dfs(db, "edges", node, visitedNodes)
+		}
+	}
+	fmt.Printf("Number of subgraphs: %d\n", subgraphCount)
+
  }
+
+// dfs performs depth-first search on the graph starting from a given node
+func dfs(db driver.Database, edgeCollName string, node string, visitedNodes map[string]bool) {
+	visitedNodes[node] = true
+	query := fmt.Sprintf("FOR v, e IN ANY '%s' %s RETURN v._id", node, edgeCollName)
+	cursor, err := db.Query(context.Background(), query, nil)
+	//fmt.Println(query)
+	if err != nil {
+		panic(err)
+	}
+	defer cursor.Close()
+	for {
+		var neighbor string
+		_, err := cursor.ReadDocument(context.Background(), &neighbor)
+		//fmt.Println(neighbor)
+		//fmt.Println(err)
+		if driver.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		if !visitedNodes[neighbor] {
+			dfs(db, edgeCollName, neighbor, visitedNodes)
+		}
+	}
+}
+
+// createsCycle checks if connecting two nodes will create a cycle in the graph
+func createsCycle(db driver.Database, edgeCollName string, fromNode string, toNode string) bool {
+	visitedNodes := make(map[string]bool)
+	visitedNodes[fromNode] = true
+	query := fmt.Sprintf(`
+		FOR v, e IN ANY '%s' %s
+			FILTER e._to == '%s'
+			RETURN v._id
+	`, fromNode, edgeCollName, toNode)
+	cursor, err := db.Query(context.Background(), query, nil)
+	//fmt.Println(query)
+	if err != nil {
+		panic(err)
+	}
+	defer cursor.Close()
+	if cursor.Count() > 0 {
+		return true
+	}
+	return false
+}
